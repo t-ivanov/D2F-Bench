737129acf068a61d8a9c528971e4705dbcef3360
t-ivanov <todor.v.ivanov@gmail.com>
2015-08-11 19:17:55 +0000

    added exec time

--- bin/tpch-exec-queries.sh
+++ bin/tpch-exec-queries.sh
@@ -15,6 +15,20 @@ QUERY_DIR=$BENCH_HOME/$BENCHMARK/queries
 CURDATE="`date +%Y-%m-%d`"
 RESULT_DIR=$BENCH_HOME/$BENCHMARK/results/$CURDATE
 mkdir $RESULT_DIR
+# Initialize log file for data loading times
+LOG_FILE_EXEC_TIMES="${BENCH_HOME}/${BENCHMARK}/logs/query_times.csv"
+if [ ! -e "$LOG_FILE_EXEC_TIMES" ]
+  then
+    touch "$LOG_FILE_EXEC_TIMES"
+    echo "STARTDATE_EPOCH|STOPDATE_EPOCH|DURATION_MS|STARTDATE|STOPDATE|DURATION|BENCHMARK|DATABASE|SCALE_FACTOR|FILE_FORMAT|QUERY" >> "${LOG_FILE_EXEC_TIMES}"
+fi
+
+if [ ! -w "$LOG_FILE_EXEC_TIMES" ]
+  then
+    echo "ERROR: cannot write to: $LOG_FILE_EXEC_TIMES, no permission"
+    return 1
+fi
+
 
 HOSTFILE=$BENCH_HOME/bin/hostlist
 i=1
@@ -25,7 +39,24 @@ do
 	  echo "Drop all caches from all nodes list in ${HOSTFILE}"
   	$BENCH_HOME/bin/drop-cache-all.sh $HOSTFILE
 	fi
+	
+	# Measure time for query execution time
+	# Start timer to measure data loading for the file formats
+	STARTDATE="`date +%Y/%m/%d:%H:%M:%S`"
+	STARTDATE_EPOCH="`date +%s`" # seconds since epoch
+	# Execute query
 	hive -i ${HIVE_SETTING} --database ${DATABASE} -f ${QUERY_DIR}/tpch_query${i}.sql > ${RESULT_DIR}/${DATABASE}_query${i}.txt 2>&1
+	
+	# Calculate the time
+	STOPDATE="`date +%Y/%m/%d:%H:%M:%S`"
+	STOPDATE_EPOCH="`date +%s`" # seconds since epoch
+	DIFF_s="$(($STOPDATE_EPOCH - $STARTDATE_EPOCH))"
+	DIFF_ms="$(($DIFF_s * 1000))"
+	DURATION="$(($DIFF_s / 3600 ))h $((($DIFF_s % 3600) / 60))m $(($DIFF_s % 60))s"
+	# log the times in load_time.csv file
+	echo "${STARTDATE_EPOCH}|${STOPDATE_EPOCH}|${DIFF_ms}|${STARTDATE}|${STOPDATE}|${DURATION}|${BENCHMARK}|${DATABASE}|${SCALE}|${FILE_FORMAT}|Query ${i}" >> ${LOG_FILE_EXEC_TIMES}
+
+	
 	#i=$(( i+1 ))	
 	i=`expr $i + 1`
 done
